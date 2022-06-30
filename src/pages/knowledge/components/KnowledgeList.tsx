6409a586800fe063752d90a91516fed7f38e2480
周晨阳 <1448517833@qq.com>
2022-06-30 12:58:30 +0800

    Update KnowledgeList.tsx

--- src/pages/knowledge/components/KnowledgeList.tsx
+++ src/pages/knowledge/components/KnowledgeList.tsx
@@ -3,11 +3,23 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import data from '../data'
 
+interface DataType {
+  key: string
+  title: string
+  date: string
+  description: string
+  url?: string
+}
+
 const KnowledgeList: React.FC = () => {
   const navigate = useNavigate()
 
-  const handleClick = (key: string) => {
-    navigate(`/knowledge/${key}`, { replace: true })
+  const handleClick = (item: DataType) => {
+    if (item.url) {
+      window.open(item.url)
+    } else {
+      navigate(`/knowledge/${item.key}`, { replace: true })
+    }
   }
 
   return (
@@ -19,7 +31,7 @@ const KnowledgeList: React.FC = () => {
         <List.Item
           key={item.key}
           extra={item.date}
-          onClick={() => handleClick(item.key)}
+          onClick={() => handleClick(item)}
         >
           <List.Item.Meta title={item.title} description={item.description} />
         </List.Item>
